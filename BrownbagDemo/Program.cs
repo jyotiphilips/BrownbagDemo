33848fc652d4f351b531f4ca718024b54a041aec
usd21544 <jyoti.kumari@philips.com>
2016-08-30 22:49:29 -0700

    adding brownbag demo

---
+++ BrownbagDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BrownbagDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello All!");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
